1a8fb99258ac8c3ad4e6cdc7b48e30811ef2cb97
diwaperkasa <diwa.perkasa@gmail.com>
2025-07-30 12:43:56 +0700

    fix double viewport

--- header.php
+++ header.php
@@ -10,7 +10,6 @@ use PHP_CodeSniffer\Reports\Json;
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
